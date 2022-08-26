d4dbdd88e3654c2a31479a7f89b5f9ac1d6d61bc
dfish19 <dfish19@yahoo.com>
2022-08-26 11:38:55 -0500

    initial

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,5 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+List<string> colors = new List<string>() {"green", "yellow", "blue" };
+colors.OrderBy(color => color.Length).ToList().ForEach(color => Console.WriteLine(color));
